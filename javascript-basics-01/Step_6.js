5a401021bee8e479b2929324f8e2c84b749c0ef3
zahraa mazloum <zahraamazloum2001@gmail.com>
2022-12-07 20:08:23 +0200

    Step_6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,12 @@
+let n =prompt("Enter a number");
+document.getElementById("first_number").value=n;
+let s =prompt("Enter a second number");
+document.getElementById("second_number").value=s;
+let z = n/s; //division of two numbers
+let mul =n%s ;
+let validate=document.getElementById("validate");
+validate.onclick= valid
+function valid(){
+  alert(mul)
+}
+
